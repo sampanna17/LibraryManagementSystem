dbc127bdaa816786fdf2d67c5b6aa2c1483b3c0d
sampanna17 <sampannapiya13@gmail.com>
2026-06-12 11:17:33 +0545

    Member Model Creation

--- src/main/java/com/librarymanagementsystem/model/Member.java
+++ src/main/java/com/librarymanagementsystem/model/Member.java
@@ -1,4 +1,25 @@
 package com.librarymanagementsystem.model;
 
 public class Member {
+
+    private int id;
+    private String name;
+
+    public Member(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return "ID : " + id +" | Name: "+ name;
+    }
 }
