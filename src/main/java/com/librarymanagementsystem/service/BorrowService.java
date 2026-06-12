fa19fa1275d5e2229fc5abc9f1b9bd5b8563c4d2
sampanna17 <sampannapiya13@gmail.com>
2026-06-12 14:06:58 +0545

    Member Service Creation

--- src/main/java/com/librarymanagementsystem/service/BorrowService.java
+++ src/main/java/com/librarymanagementsystem/service/BorrowService.java
@@ -1,4 +1,26 @@
 package com.librarymanagementsystem.service;
 
+import com.librarymanagementsystem.model.Member;
+import com.librarymanagementsystem.repository.MemeberRepository;
+
+import java.awt.desktop.SystemEventListener;
+
 public class BorrowService {
+
+    private final MemeberRepository repository = new MemeberRepository();
+
+    public void addMember(Member member){
+        repository.save(member);
+    }
+
+    public void displayMembers(){
+        for (Member member: repository.findAll()){
+            System.out.println(member);
+        }
+    }
+
+    public Member findMember(int id){
+        return repository.findById(id);
+    }
+
 }
