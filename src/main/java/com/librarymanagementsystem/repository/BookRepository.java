aa819308eaafdf1e43cb39d5211215562483fd74
sampanna17 <sampannapiya13@gmail.com>
2026-06-12 11:31:29 +0545

    Book Repository Creation

--- src/main/java/com/librarymanagementsystem/repository/BookRepository.java
+++ src/main/java/com/librarymanagementsystem/repository/BookRepository.java
@@ -1,4 +1,27 @@
 package com.librarymanagementsystem.repository;
 
+import com.librarymanagementsystem.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class BookRepository {
+
+    // Stores all books in memory
+    private final List<Book> books = new ArrayList<>();
+
+    public void save(Book book){
+        books.add(book);
+    }
+
+    public List<Book> findAll() {
+        return books;
+    }
+
+    public Optional<Book> findById(int id){
+        return books.stream()
+                .filter(book -> book.getId() == id)
+                .findFirst();
+    }
 }
