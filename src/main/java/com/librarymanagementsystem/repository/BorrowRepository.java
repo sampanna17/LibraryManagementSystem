6c5fbfd8f2f59c978269cfe21e90bf37e40b8fa7
sampanna17 <sampannapiya13@gmail.com>
2026-06-12 13:37:00 +0545

    Borrow records repository creation

--- src/main/java/com/librarymanagementsystem/repository/BorrowRepository.java
+++ src/main/java/com/librarymanagementsystem/repository/BorrowRepository.java
@@ -1,4 +1,20 @@
 package com.librarymanagementsystem.repository;
 
+import com.librarymanagementsystem.model.BorrowRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BorrowRepository {
+
+    private final  List<BorrowRecord> records = new ArrayList<>();
+
+    public void save(BorrowRecord record){
+        records.add(record);
+    }
+
+    public List<BorrowRecord> findAll(){
+        return records;
+    }
+
 }
