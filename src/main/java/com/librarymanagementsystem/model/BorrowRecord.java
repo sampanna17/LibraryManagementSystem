a08c80faa0a76c943fad9263f62e01af9a6ec8d3
sampanna17 <sampannapiya13@gmail.com>
2026-06-12 11:21:31 +0545

    Borrow record Model Creation

--- src/main/java/com/librarymanagementsystem/model/BorrowRecord.java
+++ src/main/java/com/librarymanagementsystem/model/BorrowRecord.java
@@ -1,4 +1,20 @@
 package com.librarymanagementsystem.model;
 
 public class BorrowRecord {
+
+    private int bookId;
+    private int memberId;
+
+    public BorrowRecord(int bookId , int memberId){
+        this.bookId =  bookId;
+        this.memberId = memberId;
+    }
+
+    public int getBookId(){
+        return  bookId;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
 }
