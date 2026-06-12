b5b4aacee3cc22d9df62f2495e047fc338675452
sampanna17 <sampannapiya13@gmail.com>
2026-06-12 11:14:03 +0545

    Book Model Creation

--- src/main/java/com/librarymanagementsystem/model/Book.java
+++ src/main/java/com/librarymanagementsystem/model/Book.java
@@ -1,4 +1,44 @@
 package com.librarymanagementsystem.model;
 
 public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book (int id, String title, String author){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = true;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available){
+        this.available =  available;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(
+            "ID: %d | Title: %s | Author: %s  | Available: %s",
+            id, title, author, available ? "Yes" : "No"
+        );
+    }
 }
