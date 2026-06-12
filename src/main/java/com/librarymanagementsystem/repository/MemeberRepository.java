29dc02ddf8537c5e6d1ba0e1c33e0ed08774e1e1
sampanna17 <sampannapiya13@gmail.com>
2026-06-12 11:50:33 +0545

    Member Repository Creation

--- src/main/java/com/librarymanagementsystem/repository/MemeberRepository.java
+++ src/main/java/com/librarymanagementsystem/repository/MemeberRepository.java
@@ -1,4 +1,27 @@
 package com.librarymanagementsystem.repository;
 
+import com.librarymanagementsystem.model.Member;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemeberRepository {
+
+    private final List<Member> members = new ArrayList<>();
+
+    public void save(Member member){
+        members.add(member);
+    }
+
+    public List<Member> findAll() {
+        return members;
+    }
+
+    public Member findById(int id){
+        for (Member member : members){
+            if(member.getId() == id){
+                return member;
+            }
+        }
+        return null;
+    }
 }
